4e4cbbc6bd94668337263569c9bc7692b56d918d
Aleksandr232 <aleksrowing232@gmail.com>
2024-08-08 02:06:26 +0300

    modal

--- resources/views/admin/inc/modal.blade.php
+++ resources/views/admin/inc/modal.blade.php
@@ -103,10 +103,14 @@
               <input type="number" name="price" class="form-control" id="productPrice" placeholder="Введите цену">
             </div>
             <div class="mb-3">
+                <label for="productPrice" class="form-label">Описание товара</label>
+                <textarea name="desc_product" id="summernote" cols="30" rows="10"></textarea>
+
+              </div>
+            <div class="mb-3">
                 <label for="productPrice" class="form-label">Фото товара</label>
                 <input type="file" multiple name="product_img[]" class="form-control" id="productPrice" placeholder="Введите цену">
               </div>
-              {{-- <div id="summernote"><p>Hello Summernote</p></div> --}}
             <button type="submit" class="btn btn-primary">Сохранить</button>
           </form>
         </div>
