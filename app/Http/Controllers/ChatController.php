a482464b207148cdc8eac6b41ac712da8d0079f3
Aleksandr232 <aleksrowing232@gmail.com>
2024-08-10 03:26:13 +0300

    chat

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Message;
+use App\Events\MessageSent;
 
 class ChatController extends Controller
 {
