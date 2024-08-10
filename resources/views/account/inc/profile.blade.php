d882291dbdfcdf60fe9cd6943f71974702340733
Aleksandr232 <aleksrowing232@gmail.com>
2024-08-10 03:43:31 +0300

    form

--- resources/views/account/inc/profile.blade.php
+++ resources/views/account/inc/profile.blade.php
@@ -186,7 +186,7 @@
             </div>
             <div class="chat_footer">
                 <span>
-                    <input class="chat_footer-text" type="text" placeholder="Напишите продавцу перед оплатой">
+                    {{-- <input class="chat_footer-text" type="text" placeholder="Напишите продавцу перед оплатой">
                     <div class="image-upload">
                         <label for="file-input">
                             <img src="{{ asset('site/assets/images/gallery.svg')}}"/>
@@ -194,8 +194,8 @@
 
                         <input id="file-input" type="file"/>
                     </div>
-                    <button>Відправити</button>
-                    {{-- <form method="post" action="{{route('sendMessage')}}">
+                    <button>Відправити</button> --}}
+                    <form method="post" action="{{route('sendMessage')}}">
                         @csrf
                         <input name="message" class="chat_footer-text" type="text" placeholder="Напишите продавцу перед оплатой">
                         <div class="image-upload">
@@ -205,7 +205,7 @@
                           <input id="file-input" type="file"/>
                         </div>
                         <button type="submit">Відправити</button>
-                      </form> --}}
+                      </form>
                 </span>
             </div>
         </div>
