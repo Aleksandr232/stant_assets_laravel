2516d828e334d8ad2b4dcbb56e9010fca50544ff
Aleksandr232 <aleksrowing232@gmail.com>
2024-08-13 01:16:04 +0300

    main

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -4,16 +4,22 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Broadcast;
 use Illuminate\Support\ServiceProvider;
+use App\Broadcasting\ChatChannel;
 
 class BroadcastServiceProvider extends ServiceProvider
 {
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    /* public function boot(): void
     {
         Broadcast::routes();
 
         require base_path('routes/channels.php');
-    }
+    } */
+
+    public function boot()
+       {
+           Broadcast::channel('private-chat.{recipientId}', ChatChannel::class);
+       }
 }
