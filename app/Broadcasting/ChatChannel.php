0cdc20971d7b1d21019da677efecf894b04d833e
Aleksandr232 <aleksrowing232@gmail.com>
2024-08-13 01:10:09 +0300

    chat

--- app/Broadcasting/ChatChannel.php
+++ app/Broadcasting/ChatChannel.php
@@ -17,9 +17,19 @@ class ChatChannel
     /**
      * Authenticate the user's access to the channel.
      */
-    public function join($user, $recipientId)
-       {
-           $recipient = User::findOrFail($recipientId);
-           return $user->canChatWith($recipient);
-       }
+    public function join(User $user): array|bool
+    {
+        // Проверяем, авторизован ли пользователь
+        if (!auth()->check()) {
+            return false;
+        }
+
+
+
+        // Возвращаем данные пользователя
+        return [
+            'id' => $user->id,
+            'name' => $user->name
+        ];
+    }
 }
