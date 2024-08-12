697f7118518bf574ee5be60471fdc31e432510be
Aleksandr232 <aleksrowing232@gmail.com>
2024-08-12 18:07:02 +0300

    sidebar

--- resources/views/admin/inc/sidebar.blade.php
+++ resources/views/admin/inc/sidebar.blade.php
@@ -4,7 +4,7 @@
       <li><a href="{{ route('admin') }}"><i class="fas fa fa-home me-2"></i>Главная</a></li>
       <li><a href="{{ route('product.index') }}"><i class="fas fa-store me-2"></i>Товары</a></li>
       <li><a href="{{ route('users.index') }}"><i class="fas fa-user me-2"></i>Пользователи</a></li>
-      <li><a href="{{ route('caht.index') }}"><i class="fas fa-user me-2"></i>Чат</a></li>
+      <li><a href="{{ route('chat.index') }}"><i class="fas fa-user me-2"></i>Чат</a></li>
       <li><a href="{{ route('blog.index') }}"><i class="fas fa-pencil me-2"></i> Блог</a></li>
       <li><a href="{{ route('politics.index') }}"><i class="fas fa-edit me-2"></i> Политика</a></li>
       <li><a href="#"><i class="fas fa-language me-2"></i>Языки</a></li>
