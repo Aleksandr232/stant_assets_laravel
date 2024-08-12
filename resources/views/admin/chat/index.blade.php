2985d8bb3325195bba5dd604a08a87e4072a798f
Aleksandr232 <aleksrowing232@gmail.com>
2024-08-12 18:24:55 +0300

    index

--- resources/views/admin/chat/index.blade.php
+++ resources/views/admin/chat/index.blade.php
@@ -4,59 +4,57 @@
 
 @section('content')
 <div class="row mb-4 justify-content-center mt-5">
-    <section class="profile">
-
-
-            <div  class="chat_list">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-4 chat_list">
                 @php
                     $authUsersId = auth()->user()->id;
                 @endphp
                 @foreach($chat as $post)
-                <a href="" class="chat_list-item" data-user-id="{{ $post->id }}" data-auth-id="{{ $authUsersId }}">
-                    <span class="chat_list-item-left">
-                        <img src="./assets/images/Ellipse 2.png"/>
-                        <span>
-                            <label>{{ $post->name }}</label>
-                            <p>Dinner</p>
-                        </span>
-                    </span>
-                    <span class="chat_list-item-right">
+                <a href="#" class="chat_list-item d-flex align-items-center" data-user-id="{{ $post->id }}" data-auth-id="{{ $authUsersId }}">
+                    <div class="chat_list-item-left d-flex align-items-center">
+                        <img src="./assets/images/Ellipse 2.png" class="rounded-circle mr-3"/>
+                        <div>
+                            <h6>{{ $post->name }}</h6>
+                            <p class="mb-0">Dinner</p>
+                        </div>
+                    </div>
+                    <div class="chat_list-item-right d-flex flex-column align-items-end">
                         <span>Today, 8:56pm</span>
-                        <div class="chat_list-item-right-messages">2</div>
-                    </span>
+                        <div class="chat_list-item-right-messages badge badge-primary">2</div>
+                    </div>
                 </a>
                 @endforeach
             </div>
-
-            <div class="chat profile_chat">
-                <div class="chat_header">
-
-                <span class="chat_header_to">
-                    {{-- <img src="https://content.freelancehunt.com/profile/photo/225/bonzaznob.png"/> --}}
-                    <span>Admin</span>
-                </span>
-                <span class="chat_header-underline"></span>
+            <div class="col-8 chat profile_chat">
+                <div class="chat_header d-flex align-items-center">
+                    <div class="chat_header_to d-flex align-items-center">
+                        <img src="https://content.freelancehunt.com/profile/photo/225/bonzaznob.png" class="rounded-circle mr-3"/>
+                        <span>Admin</span>
+                    </div>
+                    <div class="chat_header-underline flex-grow-1"></div>
                 </div>
                 <div class="chat_main">
                     <div class="chat_main_to"></div>
                     <div class="chat_main_from"></div>
                 </div>
                 <div class="chat_footer">
-                    <span>
-                        <input class="chat_footer-text" id="message" type="text" placeholder="Напишите продавцу перед оплатой">
-                        <div class="image-upload">
-                            <label for="file-input">
+                    <div class="input-group">
+                        <input class="form-control chat_footer-text" id="message" type="text" placeholder="Напишите продавцу перед оплатой">
+                        <div class="input-group-append">
+                            <label for="file-input" class="btn btn-outline-secondary">
                                 <img src="{{ asset('site/assets/images/gallery.svg')}}"/>
                             </label>
-
-                            {{-- <input id="file-input" type="file"/> --}}
+                            <input id="file-input" type="file" class="d-none">
+                            <button class="btn btn-primary" id="send-button">Відправити</button>
                         </div>
-                        <button id="send-button">Відправити</button>
-                    </span>
+                    </div>
                 </div>
             </div>
+        </div>
+    </div>
 
-        </section>
+
 
   </div>
 @endsection
