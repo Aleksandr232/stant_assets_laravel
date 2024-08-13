b0607ff2768bc5e3ee3533c0101d2f85e045f38e
Aleksandr232 <aleksrowing232@gmail.com>
2024-08-13 11:20:00 +0300

    sent

--- app/Events/MessageSent.php
+++ app/Events/MessageSent.php
@@ -40,6 +40,6 @@ class MessageSent implements ShouldBroadcast
      */
     public function broadcastOn()
     {
-        return new PrivateChannel('chat.' . $this->recipientId);
+        return new Channel('chat.' . $this->recipientId);
     }
 }
