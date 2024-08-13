6b23cf44c2c03c62e2098fd22a3db7ede770515f
Aleksandr232 <aleksrowing232@gmail.com>
2024-08-13 03:08:23 +0300

    main

--- resources/views/site/layouts/main.blade.php
+++ resources/views/site/layouts/main.blade.php
@@ -185,7 +185,7 @@ window.onclick = function(event) {
 
             // Получение сообщений в реальном времени
             /* var channel = pusher.subscribe('chat.' + currentActiveUserId + '-' + authId); */
-            var channel = pusher.subscribe('chat.' + currentActiveUserId);
+            /* var channel = pusher.subscribe('chat.' + currentActiveUserId);
             channel.bind('App\\Events\\MessageSent', function(data) {
                 console.log('Received data:', data);
                 addMessageToChat(data);
@@ -194,7 +194,27 @@ window.onclick = function(event) {
             pusher.trigger('chat.' + currentActiveUserId, 'App\\Events\\MessageSent', function(data) {
                 console.log('trigger:', data);
                 addMessageToChat(data);
+            }); */
+
+            var channel = pusher.subscribe('chat.' + currentActiveUserId);
+            channel.bind('App\\Events\\MessageSent', function(data) {
+                console.log('Received data:', data);
+                addMessageToChat(data);
             });
+
+            var message = messageInput.value.trim();
+            if (message) {
+                pusher.trigger('chat.' + currentActiveUserId, 'App\\Events\\MessageSent', {
+                    message: message,
+                    user_id: currentActiveUserId
+                });
+                messageInput.value = '';
+                // Add the message to the chat UI
+                addMessageToChat({
+                    message: message,
+                    user_id: currentActiveUserId
+                });
+            }
 
 
         });
