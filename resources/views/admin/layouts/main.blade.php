0cc87305879ce4b9efcd0aad9418ff0bc989c99a
Aleksandr232 <aleksrowing232@gmail.com>
2024-08-14 10:33:08 +0300

    main

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -249,7 +249,7 @@
 
             // Получение сообщений в реальном времени
             /* var channel = pusher.subscribe('chat.' + currentActiveUserId + '-' + authId); */
-            var channel = pusher.subscribe('private-chat.' + currentActiveUserId);
+            var channel = pusher.subscribe('private-chat.' + currentActiveUserId + '.' + authId);
             channel.bind('App\\Events\\MessageSent', function(data) {
                     console.log('Received data:', data.message);
                     addMessageToChat(data);
