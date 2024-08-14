7d4cbb4c91fb67177099b04471f31c63d7773ef5
Aleksandr232 <aleksrowing232@gmail.com>
2024-08-14 03:08:20 +0300

    main

--- resources/views/site/layouts/main.blade.php
+++ resources/views/site/layouts/main.blade.php
@@ -173,6 +173,6 @@ $(document).ready(function() {
     });
 });
 </script>
-<script src="{{ mix('/js/app.js') }}" defer></script>
+<script src="{{ asset('js/app.js') }}" defer></script>
 </body>
 </html>
