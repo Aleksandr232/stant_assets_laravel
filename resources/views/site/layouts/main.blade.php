37a3c94931bb93e373912e61792ee96cd0f6488f
Aleksandr232 <aleksrowing232@gmail.com>
2024-08-14 20:53:30 +0300

    main

--- resources/views/site/layouts/main.blade.php
+++ resources/views/site/layouts/main.blade.php
@@ -251,7 +251,7 @@ window.onclick = function(event) {
         // Check if the message is from the current user
         if (data.message.user_id === authId) {
             chatElement = $('<div class="chat_main_to"></div>');
-        } else {
+        } else if(data.message.user_id === data.message.recipient_id) {
             chatElement = $('<div class="chat_main_from"></div>');
 
         }
