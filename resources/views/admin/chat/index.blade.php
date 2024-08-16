4b75e9255307f5b0288e05f138bbdafa4620a1cc
Aleksandr232 <aleksrowing232@gmail.com>
2024-08-16 16:23:16 +0300

    index

--- resources/views/admin/chat/index.blade.php
+++ resources/views/admin/chat/index.blade.php
@@ -31,6 +31,12 @@
             <div class="chat_header">
 
             <span class="chat_header_to">
+                <div class="arrow-back">
+                    <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                      <line x1="19" y1="12" x2="5" y2="12"></line>
+                      <polyline points="12 19 5 12 12 5"></polyline>
+                    </svg>
+                  </div>
                 {{-- <img src="https://content.freelancehunt.com/profile/photo/225/bonzaznob.png"/> --}}
                 <span>Admin</span>
             </span>
