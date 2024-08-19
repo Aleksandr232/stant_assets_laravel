53d7280b43dcbcd15f996850fc3acc03f280aea5
Aleksandr232 <aleksrowing232@gmail.com>
2024-08-19 18:02:39 +0300

    modal

--- resources/views/account/inc/modal_avatar.blade.php
+++ resources/views/account/inc/modal_avatar.blade.php
@@ -4,7 +4,7 @@
       <h2>Добавить аватар</h2>
       <div class="user-image">
         <img src="{{ asset('site/assets/images/profile/profileIcon.png')}}" alt="Аватар пользователя" id="user-avatar">
-        <label for="avatar-input" class="upload-button">Загрузить фото</label>
+        {{-- <label for="avatar-input" class="upload-button">Загрузить фото</label> --}}
         <input type="file" id="avatar-input" class="hidden">
       </div>
       <button id="save-avatar">Сохранить</button>
