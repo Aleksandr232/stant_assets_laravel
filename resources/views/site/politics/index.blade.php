b346b2100da72135bec1249ae625adfba65e53eb
Aleksandr232 <aleksrowing232@gmail.com>
2024-08-20 18:26:03 +0300

    index

---
+++ resources/views/site/politics/index.blade.php
@@ -0,0 +1,17 @@
+@extends('site.layouts.main')
+
+@section('title', $politics->id)
+
+@section('description', 'WebSite')
+
+@section('content')
+    @include('site.inc.main')
+@endsection
+
+@section('footer_desc')
+    @include('site.inc.footer__home_desc')
+@endsection
+
+@section('footer')
+    @include('site.inc.footer_home')
+@endsection
