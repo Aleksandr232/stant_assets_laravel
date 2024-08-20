8f6c7247c91458c1b7e14b8395845f7596cecfe2
Aleksandr232 <aleksrowing232@gmail.com>
2024-08-20 20:44:17 +0300

    main

--- resources/views/site/layouts/main.blade.php
+++ resources/views/site/layouts/main.blade.php
@@ -12,6 +12,7 @@
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
     <!-- Add the slick-theme.css if you want default styling -->
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
 </head>
 
 <body>
@@ -30,6 +31,7 @@
         @include('site.inc.right__socials')
     </div>
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
     <script src="https://js.pusher.com/7.0/pusher.min.js"></script>
--- resources/views/site/politics/index.blade.php
+++ resources/views/site/politics/index.blade.php
@@ -4,9 +4,16 @@
 
 @section('description', 'WebSite')
 
-{{-- @section('content')
-    @include('site.inc.main')
-@endsection --}}
+@section('content')
+<div class="container content-politics">
+    <div class="row">
+        <div class="col-12 col-md-8 offset-md-2">
+            <h1 class="mb-4">Политика конфиденциальности</h1>
+            {!! $politics->content_politics !!}
+        </div>
+    </div>
+</div>
+@endsection
 
 @section('footer_desc')
     @include('site.inc.footer__home_desc')
