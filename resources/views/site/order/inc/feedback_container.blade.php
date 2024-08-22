0fb26d8d97742e5cc7b1f9c37d0cb88e7ac3a98e
Aleksandr232 <aleksrowing232@gmail.com>
2024-08-22 18:32:44 +0300

    feedback

--- resources/views/site/order/inc/feedback_container.blade.php
+++ resources/views/site/order/inc/feedback_container.blade.php
@@ -92,6 +92,39 @@
         </div>
 
     </div>
+        <div class="chat">
+            <div class="chat_header">
+
+                <span class="chat_header_to">
+                    <div class="arrow-back">
+                        <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                          <line x1="19" y1="12" x2="5" y2="12"></line>
+                          <polyline points="12 19 5 12 12 5"></polyline>
+                        </svg>
+                      </div>
+                    {{-- <img src="https://content.freelancehunt.com/profile/photo/225/bonzaznob.png"/> --}}
+                    <span></span>
+                </span>
+                <span class="chat_header-underline"></span>
+                </div>
+                <div class="chat_main">
+                    <div class="chat_main_to"></div>
+                    <div class="chat_main_from"></div>
+                </div>
+                <div class="chat_footer">
+                    <span>
+                        <input class="chat_footer-text" id="message" type="text" placeholder="Напишите продавцу перед оплатой">
+                        <div class="image-upload">
+                            <label for="file-input">
+                                <img src="{{ asset('site/assets/images/gallery.svg')}}"/>
+                            </label>
+
+                            {{-- <input id="file-input" type="file"/> --}}
+                        </div>
+                        <button id="send-button">Відправити</button>
+                    </span>
+                </div>
+        </div>
 
     {{-- <div class="chat">
         <div class="chat_header">
