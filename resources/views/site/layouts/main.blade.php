d69a8b98a64a17a925c3f9bb1b10b6de62cd0ce9
Aleksandr232 <aleksrowing232@gmail.com>
2024-08-25 16:01:47 +0300

    main

--- resources/views/site/layouts/main.blade.php
+++ resources/views/site/layouts/main.blade.php
@@ -368,7 +368,7 @@ $.ajax({
 
             // Обновляем ссылку на оформление заказа для текущего продукта
             var orderLink = $('.container_products_list-item:last .item_order-take');
-            orderLink.attr('href', '{{ route('order', ['id', 'product']) }}'.replace('id', product.id).replace('product', product.product));
+            orderLink.attr('href', '{{ route('order', ['id' => product.id, 'product' => product.product]) }}'.replace('id', product.id).replace('product', product.product));
         });
 
         // Добавляем класс 'active' для первого продукта
@@ -481,7 +481,7 @@ function createProductHtml(product) {
                         </td>
                         <td>
                             <div class="item_order">
-                                
+
                                 <a href="" class="item_order-contact">
                                     <span>
                                         Связаться с нами
