248bd9a27c0ed5292f9b7682a90390f4490148b3
Aleksandr232 <aleksrowing232@gmail.com>
2024-08-25 15:36:13 +0300

    main

--- resources/views/site/layouts/main.blade.php
+++ resources/views/site/layouts/main.blade.php
@@ -483,7 +483,7 @@ function createProductHtml(product) {
                         </td>
                         <td>
                             <div class="item_order">
-                                <a href="#" class="item_order-take" data-id="{{ $post->id }}" data-product="{{ $post->product }}">
+                                <a href="#" class="item_order-take" data-id="${product.id}" data-product="${product.product}">
                                     <span>Оформление заказа</span>
                                 </a>
                                 <a href="" class="item_order-contact">
