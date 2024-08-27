c5287971b6dec3272b045a7d6cdd42fe9710668e
Aleksandr232 <aleksrowing232@gmail.com>
2024-08-28 01:51:55 +0300

    main

--- resources/views/site/layouts/main.blade.php
+++ resources/views/site/layouts/main.blade.php
@@ -511,7 +511,7 @@ $.ajax({
 
 $('#search-input').on('input', function() {
     var search = $(this).val();
-    loadProductsWithSearch(1, search,  minPrice='', maxPrice='', filterPrice='', platform='');
+    loadProductsWithSearch(1, search, '', '', '', '');
 });
 
 $('#slider-2').on('input', function() {
